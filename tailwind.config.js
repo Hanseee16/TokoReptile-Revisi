cfdef4f07e476b661b4c92d2c9fd5ec922aaf206
Hanseee16 <kamilfarhan223@gmail.com>
2023-04-25 05:27:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      hijau: "#0D7524",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
